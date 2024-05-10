da78a90020545e168b54b1f07edd4f01a9aa3d38
Madson Serafim <72467045+PascalMads@users.noreply.github.com>
2024-05-10 17:03:38 -0300

    Add files via upload
    Corrigir exlog9.py!!!!!

---
+++ exlog9.py
@@ -0,0 +1,14 @@
+#Faça um algoritmo para ler três números e se estes poderem formar um triângulo dizer se o triângulo é “EQUILÁTERO”, “ISÓSCELES” OU “ESCALENO”
+print("digite um número A:")
+a = float(input())
+print("digite outro número B:")
+b = float(input())
+print("digite um número C:")
+c = float(input())
+if a == b == c:
+    print("Triângulo Equilátero")
+elif a == b != c and a == c != b and b == c != a:
+     print("Triângulo Isóceles")
+elif a != b !=c:
+    print("triângulo Escaleno")
+    
